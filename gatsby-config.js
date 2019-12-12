62c60128e1c90563b8b6727c6cbc4284761ee014
hitsuji_no_shippo <hitsujinoshippoequalsheeptail@gmail.com>
2019-12-12 14:15:18 +0900

    docs(article-example): add asciidoc examples article (close #29)

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,7 +71,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `static/images/gatsby-icon.png`,
       },
     },
     `gatsby-plugin-offline`,
