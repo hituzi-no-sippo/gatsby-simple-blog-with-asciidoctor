9e218e9ab9cd590bbc690e021827f642c0a7c41e
hitsuji_no_shippo <hitsujinoshippoequalsheeptail@gmail.com>
2019-12-19 19:20:57 +0900

    refactor(article-css): make article css a git submodule (close #70)

--- src/components/Article/index.js
+++ src/components/Article/index.js
@@ -4,9 +4,9 @@ import PropTypes from 'prop-types';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import '@fortawesome/fontawesome-free/css/v4-shims.min.css';
 
-import './common.scss'
-import './light.scss'
-import './dark.scss'
+import './scss/common.scss'
+import './scss/light.scss'
+import './scss/dark.scss'
 
 const Article = ({article}) => {
   return <article className='content' dangerouslySetInnerHTML={{ __html: article }} />
