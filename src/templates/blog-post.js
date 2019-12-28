6b65e2db9a46692be5e207f01e10aca84adde685
hitsuji_no_shippo <hitsujinoshippoequalsheeptail@gmail.com>
2019-12-28 22:17:37 +0900

    refactor(article): move code for article header to header file (close #90)

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -5,58 +5,37 @@ import { Link, graphql } from 'gatsby';
 import Bio from 'components/Bio';
 import Layout from 'components/Layout';
 import SEO from 'components/SEO';
-import TagList from 'components/TagList';
 import RelativePosts from 'components/RelativePosts';
 import Disqus from 'components/Disqus';
-import TranslationsLink from 'components/TranslationsLink';
 import Article from 'components/Article';
 
-import { formatReadingTime } from 'utils/helpers';
-import { formatDate } from 'utils/i18n';
-import { rhythm, scale } from 'utils/typography';
+import { rhythm } from 'utils/typography';
 import { useLang } from 'context/LanguageContext';
 
 function BlogPostTemplate({ data, pageContext, location }) {
   const post = data.asciidoc;
-  const siteTitle = data.site.siteMetadata.title;
   const { previous, next, previousInSameTag, nextInSameTag, translationsLink } = pageContext;
-
-  const { lang, homeLink } = useLang();
-
-  let tags;
-  if (post.pageAttributes.tags) {
-    tags = <TagList tags={post.pageAttributes.tags} baseUrl={`${homeLink}tags`} />;
-  }
+  const languageContexts = useLang();
 
   return (
-    <Layout location={location} title={siteTitle} breadcrumbs={[{ text: post.document.title }]}>
+    <Layout
+      location={location}
+      title={data.site.siteMetadata.title}
+      breadcrumbs={[{ text: post.document.title }]}
+    >
       <SEO
         title={post.document.title}
         description={post.document.description}
       />
-      <h1>{post.document.title}</h1>
-      <p
-        style={{
-          ...scale(-1 / 5),
-          display: `block`,
-          marginBottom: rhythm(1),
-          marginTop: rhythm(-1),
-        }}
-      >
-        {formatDate(post.revision.date)}
-        {` • ${formatReadingTime(post.timeToRead)}`}
-      </p>
-
-      {tags}
-      <TranslationsLink
-        translationsLink={translationsLink}
-        langKey={lang}
-        style={{ margin: '-0.5rem 0 1.5rem' }}
+      <Article
+       post={post}
+       translationsLink={translationsLink}
+       languageContexts={languageContexts}
       />
 
-      <Article article={post.html} />
-
-      <RelativePosts postNodes={[previousInSameTag, nextInSameTag]} lang={lang} />
+      <RelativePosts
+       postNodes={[previousInSameTag, nextInSameTag]}
+       lang={languageContexts.lang} />
 
       <hr
         style={{
